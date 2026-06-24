2adce44512c84562e14d93282e5b0e8d26bc8bb5
shashiquen-b-m-venencia <shashiquenvenencia@gmail.com>
2026-06-24 19:09:21 -0300

    Se agregauna función simple en las vistas Dashboardy ListaClientes solo para comprobarsi la navegación se realiza correctamente. En Login.jsx se agrega el formulario para el inicio de sesion y tambien se importa ej AdminContext donde se alojala logica de autenticación. También se integra laredireccion a la pagina principal una vez que el  usuario se loguea mediante el botón de ingreso.

---
+++ pv_tp_integrador_grupo6/src/views/ListaClientes.jsx
@@ -0,0 +1,9 @@
+const ListaClientes = () => {
+  return (
+    <div>
+      <h1>Clientes</h1>
+    </div>
+  );
+};
+
+export default ListaClientes;
---
+++ pv_tp_integrador_grupo6/src/views/Login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useAdmin } from "../context/AdminContext";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography, TextField, Button, Paper, Container, MenuItem } from '@mui/material';
+
+const Login = () => {
+    const [form, setForm] = useState({ nombre: '', sector: '' });
+    const { login } = useAdmin();
+    const navigate = useNavigate();
+
+    const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (form.nombre && form.sector) {
+            login(form.nombre, form.sector);
+            navigate("/");
+        } else {
+            alert("Completa todos los campos");
+        }
+    };
+
+    return (
+        <Container maxWidth="xs" sx={{ mt: 8 }}>
+            <Paper elevation={3} sx={{ p: 4 }}>
+                <Typography variant="h5" align="center" sx={{ mb: 3 }}>Acceso Administrador</Typography>
+                <form onSubmit={handleSubmit}>
+                    <TextField fullWidth label="Nombre" name="nombre" onChange={handleChange} margin="normal" required />
+                    <TextField fullWidth select label="Sector" name="sector" value={form.sector} onChange={handleChange} margin="normal" required>
+                        <MenuItem value="Soporte">Soporte</MenuItem>
+                        <MenuItem value="Gerencia">Gerencia</MenuItem>
+                    </TextField>
+                    <Button fullWidth variant="contained" type="submit" sx={{ mt: 3, bgcolor: '#03264e' }}>Ingresar</Button>
+                </form>
+            </Paper>
+        </Container>
+    );
+};
+
+export default Login;
