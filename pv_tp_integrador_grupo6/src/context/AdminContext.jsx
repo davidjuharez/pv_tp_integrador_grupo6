a444cee09b0ee1b980605af54d3ed6f01a47dccb
shashiquen-b-m-venencia <shashiquenvenencia@gmail.com>
2026-06-24 18:54:56 -0300

    Implementacion del AdminContext para lagestión del estado global y la autenticación donde se define el provedor del estado global para administradores. También se desarrolla la logica del Login y el logout y la persistencia de sesión y se implementa la autenticación de los usuarios.

---
+++ pv_tp_integrador_grupo6/src/context/AdminContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AdminContext = createContext();
+
+export const AdminProvider = ({ children }) => {
+    const [admin, setAdmin] = useState(null);
+
+    useEffect(() => {
+        const session = localStorage.getItem("adminSession");
+        if (session) {
+            setAdmin(JSON.parse(session));
+        }
+    }, []);
+
+    const login = (nombre, sector) => {
+        const adminData = { nombre, sector };
+        setAdmin(adminData);
+        localStorage.setItem("adminSession", JSON.stringify(adminData));
+    };
+
+    const logout = () => {
+        setAdmin(null);
+        localStorage.removeItem("adminSession");
+    };
+
+    return (
+        <AdminContext.Provider value={{ admin, login, logout, isAuthenticated: !!admin }}>
+            {children}
+        </AdminContext.Provider>
+    );
+};
+
+export const useAdmin = () => useContext(AdminContext);
+export default AdminProvider;
