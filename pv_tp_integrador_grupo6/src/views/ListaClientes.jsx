699f1904c0f5eaca843d76beaa941d2d08214085
Jonatan Cruz <anibalc279@gmail.com>
2026-06-25 18:33:30 -0300

    Creo la estructura inicial de ListaClientes y el consumo asincronico de los usuarios.

--- pv_tp_integrador_grupo6/src/views/ListaClientes.jsx
+++ pv_tp_integrador_grupo6/src/views/ListaClientes.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from 'react';
 const ListaClientes = () => {
+
+  const [clientes, setClientes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    const obtenerClientes = async () => {
+      try {
+        setLoading(true);
+        const respuesta = await fetch('https://fakestoreapi.com/users');
+        
+        if (!respuesta.ok) {
+          throw new Error('Error al conectar con el servidor');
+        }
+
+        const datos = await respuesta.json();
+        setClientes(datos);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    obtenerClientes();
+  }, []);
+
+
   return (
     <div>
       <h1>Clientes</h1>
