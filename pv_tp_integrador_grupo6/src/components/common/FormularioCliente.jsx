f013e9bdd42ec5583fbf75d3b80e07051350de56
Jonatan Cruz <anibalc279@gmail.com>
2026-06-25 21:29:44 -0300

    integro la peticion POST asincronica y feedback con Snackbar en el alta de clientes

--- pv_tp_integrador_grupo6/src/components/common/FormularioCliente.jsx
+++ pv_tp_integrador_grupo6/src/components/common/FormularioCliente.jsx
@@ -6,12 +6,57 @@ const FormularioCliente =()=>{
   const [nombre, setNombre] = useState('');
   const [apellido, setApellido] = useState('');
   const [email, setEmail] = useState('');
+  //estados para la notificación flotante de exito
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [mensajeExito, setMensajeExito] = useState('');
 
-  //esta es la función pra guardar
-  const handleSubmit = (e) => {
+  //esta es la función que maneja el envio del formulario
+    const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(`Listo para enviar: ${nombre} ${apellido} (${email})`);
 
+    //la estructura del objeto segun fakeAPI
+    const nuevoCliente = {
+      email: email,
+      username: nombre.toLowerCase(),
+      password: 'password123',
+      name: {
+        firstname: nombre,
+        lastname: apellido
+      },
+      address: {
+        city: 'Mendoza',
+        street: 'Av. San Martín',
+        number: 123,
+        zipcode: '5500',
+        geolocation: { lat: '-34.6037', long: '-58.3816' }
+      },
+      phone: '123-456-789'
+    };
+    try {
+      //esta es la petición POST
+      const respuesta = await fetch('https://fakestoreapi.com/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nuevoCliente)
+      });
+
+      if (!respuesta.ok) {
+        throw new Error('Error al registrar el cliente en el servidor');
+      }
+
+      const datos = await respuesta.json();
+      //la API nos responde con el ID
+      setMensajeExito(`¡Cliente creado con éxito! ID asignado por la API: #${datos.id}`);
+      setOpenSnackbar(true);
+      
+      setNombre('');//limpiamos los campos del formulario
+      setApellido('');
+      setEmail('');
+    } catch (error) {
+      alert('Hubo un error: ' + error.message);
+    }
   };
 
   return (
@@ -47,6 +92,17 @@ const FormularioCliente =()=>{
           Guardar Cliente
         </Button>
       </Box>
+        {/*un cartelito flotante que va aparecer abajo y se va solo*/}
+        <Snackbar
+        open={openSnackbar}
+        autoHideDuration={4000}
+        onClose={() => setOpenSnackbar(false)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+        <Alert severity="success" variant="filled" onClose={() => setOpenSnackbar(false)}>
+          {mensajeExito}
+        </Alert>
+      </Snackbar>        
+
     </Box>
   );
 };
