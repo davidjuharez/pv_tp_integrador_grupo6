6a42830c25b4f11302b8aa37523341b03c4c072b
shashiquen-b-m-venencia <shashiquenvenencia@gmail.com>
2026-07-02 16:44:46 -0300

    Envuelvo el una card el título principal del inicio y el mensaje de bienvenida, a su vez adaptamos el color de la fuente a un color que pueda ser legible y le damos a la card un tono de transparencia para que pueda verse el fondo.

--- pv_tp_integrador_grupo6/src/views/Dashboard.jsx
+++ pv_tp_integrador_grupo6/src/views/Dashboard.jsx
@@ -10,15 +10,15 @@ const Dashboard = () => {
   return (
     <Box sx={{ backgroundImage: `url(${fondoLogin})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', width: '100vw', position: 'relative', left: '50%', right: '50%', marginLeft: '-50vw', marginRight: '-50vw', marginTop: '-24px', marginBottom: '-24px', paddingTop: '24px', paddingBottom: '24px' }}>
       <Container maxWidth="lg" sx={{ mt: 5, mb: 5 }}>
-
-        <Box sx={{ mb: 6, textAlign: 'left', borderBottom: '2px solid #e0e0e0', pb: 2 }}>
-          <Typography variant="h3" sx={{ fontWeight: 800, color: '#1a237e' }}>
-            Panel de Cliente
-          </Typography>
-          <Typography variant="h6" color="text.secondary">
-            Bienvenido al panel, <strong>{admin?.nombre}</strong> | Sector: {admin?.sector}
-          </Typography>
-        </Box>
+        <Card elevation={3} sx={{ mb: 6, borderRadius: 3, bgcolor: 'rgba(3, 38, 78, 0.4)', backdropFilter: 'blur(8px)', border: '3px solid rgba(255, 255, 255, 0.2)' }}>
+          <CardContent>
+            <Box sx={{ textAlign: 'left', pb: 2 }}>
+              <Typography variant="h4" sx={{ fontWeight: 800, color: '#d6e4f0' }}>Panel de Cliente</Typography>
+              <Box sx={{ my: 2, borderBottom: '2px solid rgba(255,255,255,0.15)' }} />
+              <Typography variant="h6" sx={{ color: '#d6e4f0' }}>Bienvenido al panel, <strong>{admin?.nombre}</strong> | Sector: {admin?.sector}</Typography>
+            </Box>
+          </CardContent>
+        </Card>
 
         {/* 2. Tarjetas con separación y elevación */}
         <Grid container spacing={4}>
